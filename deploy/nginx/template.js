41560518d525e4ac77c7d0d18016ca6d4620b8e6
Pavel Nikitsiuk <p.nikitsiuk@godeltech.com>
2022-07-14 13:22:56 +0300

    add ci scripts

---
+++ deploy/nginx/template.js
@@ -0,0 +1,5 @@
+window.envConfig = {
+  ENVIRONMENT: '${ENVIRONMENT}',
+  APP_NAME: '${APP_NAME}',
+  APP_COLOR: '${APP_COLOR}'
+}
---
+++ deploy/sh/deploy-dev.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+set -xe
+
+image=$( cat image )
+
+sh ${SHARED_SCRIPTS_DIR}/sh/deploy-argocd.sh \
+  -p "applications/dev/innovations/news-client/news-client.yaml" \
+  -k "deploy/k8s/dev" \
+  -i $image
