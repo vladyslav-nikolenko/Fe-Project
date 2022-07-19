6e0705ef768c7dabfb47ddf4bfd94bfc58c1b620
Pavel Nikitsiuk <p.nikitsiuk@godeltech.com>
2022-07-19 16:07:40 +0300

    update deploy config

--- deploy/sh/deploy-dev.sh
+++ deploy/sh/deploy-dev.sh
@@ -4,6 +4,6 @@ set -xe
 image=$( cat image )
 
 sh ${SHARED_SCRIPTS_DIR}/sh/deploy-argocd.sh \
-  -p "applications/dev/innovations/news-client/news-client.yaml" \
+  -p "applications/dev/lnd/news-client/news-client.yaml" \
   -k "deploy/k8s/dev" \
   -i $image
